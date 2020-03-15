3407b0649bb0a1df24804120f84ba9a4e9d1089f
Steffi Wehrle <steffinchen@gmail.com>
2020-03-15 17:08:03 +0100

    🌐 use react-i18next for localization

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ThemeProvider from '@kiwicom/orbit-components/lib/ThemeProvider';
 import getTheme from './theme';
+import i18n from './i18n';
 
-ReactDOM.render(
-  <ThemeProvider
-    theme={{ yourCustomTheme: { black: '#000' }, orbit: getTheme() }}
-  >
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root')
+i18n.init().then(() =>
+  ReactDOM.render(
+    <ThemeProvider
+      theme={{ yourCustomTheme: { black: '#000' }, orbit: getTheme() }}
+    >
+      <App />
+    </ThemeProvider>,
+    document.getElementById('root')
+  )
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Button from '@kiwicom/orbit-components/lib/Button';
 import { Flex } from 'reflexbox';
 import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
 import Container from '../../container';
 import logo from '../../educord.png';
 
 function Main() {
+  const { t } = useTranslation();
+
   return (
     <Container>
       <Flex flexDirection="column" alignItems="center">
         <Img src={logo} alt="EduCord Logo" />
-        <h1>Welcome to EduCord</h1>
+        <h1>{t('welcome')}</h1>
         <Flex width={[1, 1 / 2]} justifyContent="space-around">
           <Button href="/teachers">I am a Teacher</Button>
           <Button href="/students">I am a Student</Button>
