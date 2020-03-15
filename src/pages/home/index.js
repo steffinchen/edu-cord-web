949d229726b701096818b9f8fbfaa43b00dd879a
Steffi Wehrle <steffinchen@gmail.com>
2020-03-15 10:07:37 +0100

    💄 favicon and logos, updated title &  description

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import Container from '../../container';
 import Button from '@kiwicom/orbit-components/lib/Button';
 import { Flex } from 'reflexbox';
+import styled from 'styled-components';
+import Container from '../../container';
+import logo from '../../educord.png';
 
 function Main() {
   return (
     <Container>
       <Flex flexDirection="column" alignItems="center">
+        <Img src={logo} alt="EduCord Logo" />
         <h1>Welcome to EduCord</h1>
         <Flex width={[1, 1 / 2]} justifyContent="space-around">
           <Button href="/teachers">I am a Teacher</Button>
@@ -16,5 +19,9 @@ function Main() {
     </Container>
   );
 }
+
+const Img = styled.img`
+  height: 64px;
+`;
 
 export default Main;
